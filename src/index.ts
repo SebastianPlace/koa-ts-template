ed9f6535af106a3a797224a0d3ea3cfbfcadab9b
Sebastian Place <sebastian@bulb.co.uk>
2021-10-24 00:35:39 +0100

    basic koa ts setup

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import Koa, { Context } from "koa";
+import bodyParser from "koa-body";
+import logger from "koa-logger";
+import Router from "koa-router";
+
+import config from "./config";
+
+const app = new Koa();
+const router = new Router();
+
+const healthHandler = (ctx: Context) => {
+  ctx.body = { ok: true };
+};
+
+router.get("/healthz", healthHandler);
+
+app.use(bodyParser());
+app.use(logger());
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(config.API_PORT, () => {
+  console.info(`App listening on port ${config.API_PORT}...`);
+});
